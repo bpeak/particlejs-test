237bc3b60580b6740da3e2d5e0730daead636f75
Jacob Kim <jacob@mykoon.com>
2019-12-12 18:11:11 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Particles from "react-particles-js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Particles
+          className="test"
+          params={{
+            particles: {
+              number: {
+                value: 50
+              },
+              size: {
+                value: 3
+              }
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "repulse"
+                }
+              }
+            }
+          }}
+        />
+        <div>ok</div>
+        <input />
+        <h1>hello particle</h1>
+        <div>ok</div>
+        <div>ok</div>
+        <input />
+        <h1>hello particle</h1>
+        <div>ok</div>
+        <div>ok</div>
+        <input />
+        <h1>hello particle</h1>
+        <div>ok</div>
+        <div>ok</div>
+        <input />
+        <h1>hello particle</h1>
+        <div>ok</div>
+        <div>ok</div>
+        <input />
+        <h1>hello particle</h1>
+        <div>ok</div>
+        <div>ok</div>
+        <input />
+        <h1>hello particle</h1>
+        <div>ok</div>
+        <div>ok</div>
+        <input />
+        <h1>hello particle</h1>
+        <div>ok</div>
+        <div>ok</div>
+        <input />
+        <h1>hello particle</h1>
+        <div>ok</div>
+        <div>ok</div>
+        <input />
+        <h1>hello particle</h1>
+        <div>ok</div>
+        <div>ok</div>
+        <input />
+        <h1>hello particle</h1>
+        <div>ok</div>
+      </>
+    );
+  }
 }
 
 export default App;
